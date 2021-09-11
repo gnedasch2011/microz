aedfb1eff248fe5a53fa2baee0ba27cf09d5a11b
Максим Гнедаш <gnedasch2011@yandex.ru>
2021-09-11 22:36:12 +0300

    reserv work

--- views/booking/successReserve.php
+++ views/booking/successReserve.php
@@ -3,6 +3,8 @@
 use app\models\booking\Rooms;
 
 ?>
+    <a href="/booking/room-search">Вернуться к поиску</a>
+
     <h1>Успешная бронь (<?= Rooms::returnName($newReserv->type_rooms); ?>):
         №<?= $newReserv->id; ?></h1>
 
